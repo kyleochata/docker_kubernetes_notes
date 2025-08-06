65e9b7e11a81a9caa80e847c4831a8b34daee6e0
kyleochata <contact.kyle.e_swe@proton.me>
2025-08-06 11:47:56 -0700

    new 1.1.1 img color-api

--- kubernetes/projects/color_api/src/index.js
+++ kubernetes/projects/color_api/src/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const os = require('os')
 
 const app = express()
 const port= 80
-
+const color = "blue"
+const hostname = os.hostname()
 app.get("/", (req, res) => {
-    res.send('<h1 style="color:blue"> Hello from Color-API </h1>')
+    res.send(`<h1 style="color:${color}"> Hello from Color-API </h1>
+<h2>Hostname: ${hostname}</h2>`)
+})
+
+app.get('/api', (req, res) => {
+    const {format} = req.query //localhost/api?format=<text | json>
+    if (format === 'json') {
+        return  res.json({
+        color,
+        hostname
+    })
+    }
+    return res.send(`Color: ${color}\t Hostname: ${hostname} `)
 })
 
 app.listen(port, () => {
