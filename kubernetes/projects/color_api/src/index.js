dd25cbaa71685db7594135daddf8309dd2b1819a
kyleochata <contact.kyle.e_swe@proton.me>
2025-08-12 18:43:43 -0700

    v1.2.0 color-api

--- kubernetes/projects/color_api/src/index.js
+++ kubernetes/projects/color_api/src/index.js
@@ -5,6 +5,15 @@ const app = express()
 const port= 80
 const color = "blue"
 const hostname = os.hostname()
+
+const delay_startup = process.env.DELAY_STARTUP === 'true'
+const fail_liveness = process.env.FAIL_LIVENESS === 'true'
+const fail_readiness = process.env.FAIL_READINESS === 'true' ? Math.random() < 0.5 : false
+
+console.log(`Delay startup: ${delay_startup}`)
+console.log(`Fail liveness: ${fail_liveness}`)
+console.log(`Fail ready: ${fail_readiness}`)
+
 app.get("/", (req, res) => {
     res.send(`<h1 style="color:${color}"> Hello from Color-API </h1>
 <h2>Hostname: ${hostname}</h2>`)
@@ -20,6 +29,30 @@ app.get('/api', (req, res) => {
     }
     return res.send(`Color: ${color}\t Hostname: ${hostname} `)
 })
+
+app.get('/ready', (req, res) => {
+    if (fail_readiness) {
+       return res.sendStatus(503) 
+    }
+    return res.send('ok')
+})
+
+app.get('/health', (req, res) => {
+    if (fail_liveness) {
+        return res.sendStatus(503)
+    }
+    return res.send('ok')
+})
+
+if (delay_startup) {
+    const start = Date.now()
+
+    //Purposefully block event loop and execution for 60s.
+    // Illustrate startup probes
+    while (Date.now() - start < 60000) {
+
+    }
+}
 
 app.listen(port, () => {
     console.log(`server connected. Listening on port: ${port}`)
